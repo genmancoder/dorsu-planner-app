010c64a8cf140300c14bf3e83daa5e7be6b37c49
John Lloyd Bonga <111741893+Johnlloydbonga@users.noreply.github.com>
2022-10-26 18:26:02 +0800

    Update Developers.tsx

--- src/pages/Developers.tsx
+++ src/pages/Developers.tsx
@@ -11,10 +11,10 @@ const Developers = () => {
                 {/* grid item */}
                 <div className="flex flex-wrap justify-center">
                     <div className="w-64 sm:w-64 px-4">
-                        <img src="https://avatars.githubusercontent.com/u/74034797?s=400&u=8d250db9bceb4f7415526ea5b276c203f10e3fa9&v=4" alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+                        <img src="https://www.facebook.com/johnlloyd.bonga.75" alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
                     </div>
-                    <h3 className='font-bold mr-1'>John Ray Paulin, </h3>
-                    <p className='font-thin italic'>Lead Developer</p>
+                    <h3 className='font-bold mr-1'>John Lloyd Bonga, </h3>
+                    <p className='font-thin italic'>Student</p>
                 </div>           
                 {/* grid item */}  
 
