c3bde461e5881b3d65dee950f2eced59de1b046c
A3RON05 <112607914+A3RON05@users.noreply.github.com>
2022-10-27 12:50:33 +0800

    Update Developers.tsx

--- src/pages/Developers.tsx
+++ src/pages/Developers.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Developers = () => {
   return (
     <div className='container mx-auto p-5'>
-        <h1>Developers</h1>
+        <h1>Ronnie Kiay Mandabon</h1>
         <div className='p-5 m-5 grid grid-cols-6 gap-2'>            
                 {/* Add your profile here. You can upload your image to the repo under "images"  directory
                     and link it here.                
@@ -11,7 +11,7 @@ const Developers = () => {
                 {/* grid item */}
                 <div className="flex flex-wrap justify-center">
                     <div className="w-64 sm:w-64 px-4">
-                        <img src="https://avatars.githubusercontent.com/u/74034797?s=400&u=8d250db9bceb4f7415526ea5b276c203f10e3fa9&v=4" alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+                        <img src="https://avatars.githubusercontent.com/u/112607914?s=40&v=4" alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
                     </div>
                     <h3 className='font-bold mr-1'>John Ray Paulin, </h3>
                     <p className='font-thin italic'>Lead Developer</p>
